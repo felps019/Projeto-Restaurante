b04952f52da785f0d83ff2e525d0b79c814c6dc1
felps019 <felipe.hsilva@kabum.com.br>
2024-05-10 14:02:12 -0300

    new project

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"], /* Qualquer arquivo na pasta raiz pode ter */
+  theme: {
+    extend: {
+      backgroundImage:{
+        "home": "url('/assets/bg.png')"
+      }
+    },
+  },
+  plugins: [],
+}
+
