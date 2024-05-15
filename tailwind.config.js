e739bb526a954f4edf4af8ddd165607ca204a339
felps019 <felipe.hsilva@kabum.com.br>
2024-05-15 11:43:43 -0300

    style.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./**/*.{html,js}"], /* Qualquer arquivo na pasta raiz pode ter */
   theme: {
+    fontFamily: {
+      'sans': ['Poppins', 'sans-serif']
+    },
     extend: {
       backgroundImage:{
         "home": "url('/assets/bg.png')"
